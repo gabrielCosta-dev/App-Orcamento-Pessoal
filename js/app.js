8e3fd045d3f07af15668e3b6aad4787a4c26e052
Gabriel Lima <lima20gabriel@gmail.com>
2023-11-06 16:14:36 -0300

    Criando Objeto Despesa

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//Variáveis Eventos
+let cadastrarDespesa = document.getElementById('cadastrarDespesa')
+
+class Despesa{
+    constructor(ano, mês, dia, tipo, descrição, valor){
+       this.ano = ano 
+       this.mês = mês
+       this.dia = dia
+       this.tipo = tipo
+       this.descrição = descrição
+       this.valor = valor
+    }
+}
+
+//Registrar Nova Despesa
+cadastrarDespesa.addEventListener('click',()=>{
+    let ano = document.getElementById('ano')
+    let mês = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descrição = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
+
+    let despesa = new Despesa(ano.value, mês.value, dia.value, tipo.value, descrição.value, valor.value)
+    console.log(despesa);
+})
